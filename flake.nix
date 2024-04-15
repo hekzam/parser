f9508c7f2ef3e985dcbf17a8fd00a0ef1d7e6176
Millian Poquet <millian.poquet@irit.fr>
2024-04-15 09:39:56 +0200

    initial setup (nix, meson, main)

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in rec {
+        packages = rec {
+          parser = pkgs.stdenv.mkDerivation {
+            pname = "hekzam-parser";
+            version = if (self ? rev) then self.shortRev else "dirty";
+            src = pkgs.lib.sourceByRegex ./. [
+              "^meson\.build"
+              "^src"
+              "^src/.*\.hpp"
+              "^src/.*\.cpp"
+            ];
+            buildInputs = with pkgs; [
+              opencv
+            ];
+            nativeBuildInputs = with pkgs; [
+              meson
+              ninja
+              pkg-config
+            ];
+          };
+          default = parser;
+        };
+        devShells = {};
+      }
+    );
+}
+
+
